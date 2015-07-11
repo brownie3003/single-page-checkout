dc0ad17e1f3aea8f283af82371cd88f60611b319
Andy Brown <brownie3003@gmail.com>
2015-07-11 22:44:34 +0100

    Docblock the address-radio component

--- app/components/address-radio.js
+++ app/components/address-radio.js
@@ -1,4 +1,12 @@
 import Ember from 'ember';
+/**
+ * A radio button for selecting an address in an address book and applying the selection to some model (e.g. order).
+ * @param Address address - instance of address model to be shown with this radio-button
+ * @param Address selectedAddress - instance of address that is set on model a Model
+ * @param String radioName - A name for the property the radio-buttons set, e.g. Shipping Address or Billing Address
+ * @property boolean isSelected - Compares whether address and selectedAddress are the same in order to check the radio button.
+ * @action setAddress - changes selectedAddress
+ */
 
 export default Ember.Component.extend({
     isSelected: Ember.computed('address', 'selectedAddress', function() {
