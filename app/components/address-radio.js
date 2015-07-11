c9eed98a66725e5559e07d6e36d7056e2ff140a0
Andy Brown <brownie3003@gmail.com>
2015-07-11 22:46:15 +0100

    fixup! Docblock the address-radio component

--- app/components/address-radio.js
+++ app/components/address-radio.js
@@ -1,6 +1,7 @@
 import Ember from 'ember';
 /**
  * A radio button for selecting an address in an address book and applying the selection to some model (e.g. order).
+ * TODO this is probably too specific for a component, I think it relies on an external model which is bad. Also is it two way data binding?
  * @param Address address - instance of address model to be shown with this radio-button
  * @param Address selectedAddress - instance of address that is set on model a Model
  * @param String radioName - A name for the property the radio-buttons set, e.g. Shipping Address or Billing Address
