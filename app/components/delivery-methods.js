1113b46102218d9d8b7dc7a6ff4c54dbc4c3b4df
Jamie White <jamie@jgwhite.co.uk>
2015-07-29 14:27:11 +0100

    Make availableDeliveryMethods lazy by decomposition

--- app/components/delivery-methods.js
+++ app/components/delivery-methods.js
@@ -1,14 +1,25 @@
 import Ember from 'ember';
+const { computed } = Ember;
 
 export default Ember.Component.extend({
     store: Ember.inject.service(),
-    availableDeliveryMethods: Ember.computed('deliveryMethods', 'shippingCountry', function() {
-        var shippingCountry = this.get('shippingCountry');
-        var store = this.get('store');
-        return store.filter('deliveryMethod', function(deliveryMethod) {
-            return deliveryMethod.get('shippingCountry') === shippingCountry;
-        })
+
+    deliveryMethods: computed('store', function() {
+        let store = this.get('store');
+
+        return store.find('delivery-method');
     }),
+
+    availableDeliveryMethods: computed(
+        'deliveryMethods.@each.shippingCountry', 'shippingCountry',
+        function() {
+            let deliveryMethods = this.get('deliveryMethods');
+            let shippingCountry = this.get('shippingCountry');
+
+            return deliveryMethods.filterBy('shippingCountry', shippingCountry);
+        }
+    ),
+
     actions : {
         setDeliveryMethod: function(deliveryMethod) {
             this.sendAction('setDeliveryMethod', deliveryMethod)
