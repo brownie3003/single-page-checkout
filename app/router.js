3d16a549db4a07dc90a9135f361babd5724d0002
Andy Brown <brownie3003@gmail.com>
2015-07-30 21:43:02 +0100

    divshot and root route

--- app/router.js
+++ app/router.js
@@ -6,7 +6,7 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-    this.route('order', { path: 'checkout' });
+    this.route('order', { path: '/' });
 });
 
 export default Router;
