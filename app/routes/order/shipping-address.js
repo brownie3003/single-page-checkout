252663aa1c2ffa94b763c21ca28a99cfeb8ec64f
Andy Brown <brownie3003@gmail.com>
2015-08-17 16:54:54 +0100

    Add shipping options and shipping address route. Not working 100% yet

--- app/routes/order/shipping-address.js
+++ app/routes/order/shipping-address.js
@@ -4,6 +4,16 @@ export default Ember.Route.extend({
     model() {
         return this.modelFor('order');
     },
+    afterModel(model) {
+        // Let's assume we can't get here without a user.
+        let user = model.get('user');
+        if (Object.keys(user.get('addressBook').getProperties()).length > 0) {
+            let defaultAddress = user.get('addressBook').get('firstObject');
+            model.set('shippingAddress', defaultAddress);
+            model.set('billingAddress', defaultAddress);
+        }
+        return model;
+    },
     setupController(controller, model) {
         controller.set("order", model);
     },
